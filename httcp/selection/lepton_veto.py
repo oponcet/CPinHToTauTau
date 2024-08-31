e9b77a26bc6b616c5e201e396eaf2756b920ef24
gourab saha <gourab.saha@cern.ch>
2024-08-31 18:44:51 +0200

    check dlveto and modify lawcfg

--- httcp/selection/lepton_veto.py
+++ httcp/selection/lepton_veto.py
@@ -137,27 +137,95 @@ def double_lepton_veto(
     double_veto_electron = ak.with_name(double_veto_electron, "PtEtaPhiMLorentzVector")
 
     # get all possible combinatorics separately for double_veto_muons and electrons
+    # mu : [ [(m11,m12),(m12,m13),(m11,m13)],
+    #        [(m21,m22)],
+    #        [],
+    #        []
+    #      ]
     mu_pair = ak.combinations(double_veto_muon, 2, axis=1)
+    # el : [ [],
+    #        [],
+    #        [e31,e32],
+    #        []
+    #      ]
     el_pair = ak.combinations(double_veto_electron, 2, axis=1)
 
     # get the components
+    # mu1 : [ [m11,m12,m11],
+    #         [m21],
+    #         [],
+    #         [],
+    #       ]
+    # mu2 : [ [m12,m13,m13],
+    #         [m22],
+    #         [],
+    #         [],
+    #       ]
     mu1,mu2 = ak.unzip(mu_pair)
+    # el1 : [ [],
+    #         [],
+    #         [e31],
+    #         [],
+    #       ]
+    # el2 : [ [],
+    #         [],
+    #         [e32],
+    #         [],
+    #       ]
     el1,el2 = ak.unzip(el_pair)
 
     # main algo
     presel_mask = lambda leps1, leps2: ((leps1.charge * leps2.charge < 0) & (leps1.delta_r(leps2) > 0.15))
 
     # dlveto masks
+    # dlveto_mu_mask : [ [True, False, False],
+    #                    [False],
+    #                    [],
+    #                    []
+    #                  ]
     dlveto_mu_mask = presel_mask(mu1,mu2)
+    # dlveto_el_mask : [ [],
+    #                    [],
+    #                    [True],
+    #                    [],
+    #                  ]
     dlveto_el_mask = presel_mask(el1,el2)
 
+    
+
+    # concatenate the masks
+    # It should not contain any True
+    # dlveto_mask : [ [True, False, False],
+    #                 [False],
+    #                 [True]
+    #               ]
+    dlveto_mask = ak.concatenate([dlveto_mu_mask, dlveto_el_mask], axis=1)
+    # this final event level mask should not conta
+    # dl_veto : [ False, True, False, True ]
+    dl_veto = ak.sum(dlveto_mask, axis=1) == 0
+    
+
     # convert to event level masks
+    ## dl_mu_veto : [ True, False, False ]
+    ## dl_mu_veto = ak.any(dlveto_mu_mask, axis=1)
+    ## dl_el_veto : [ False, False, True ]
+    ## dl_el_veto = ak.any(dlveto_el_mask, axis=1)
+
+    """
+    # dl_mu_veto : [ False, True, True, True ]
+    # That means 1st event has Z like muon pair
     dl_mu_veto = ak.sum(dlveto_mu_mask, axis=1) == 0
+    # dl_el_veto : [ True, True, False, True ]
+    # 3rd events has Z like ele pair
     dl_el_veto = ak.sum(dlveto_el_mask, axis=1) == 0
 
-    # combination of mu and el veto masks
-    dl_veto    = dl_mu_veto & dl_el_veto
 
+    # combination of mu and el veto masks
+    # So, for the example, 1st and 3rd event should be rejected
+    # dl_veto : [ False, True, False, True ]
+    dl_veto    = dl_mu_veto & dl_el_veto
+    """
+    
     return events, SelectionResult(
         steps={"dilepton_veto": dl_veto}
     )
