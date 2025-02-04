d619edaca9e670d0128507134dbe3971c43ee443
oponcet <oceane.poncet@cern.ch>
2025-02-04 16:17:43 +0100

    fake factor derivation script

---
+++ script_FF/fake_factor_derivation/src/fit_and_save_ratios.py
@@ -0,0 +1,316 @@
+import ROOT
+import pickle
+import numpy as np
+import os
+import json
+import sys
+from array import array
+from argparse import ArgumentParser
+from itertools import product  # To combine dm and n_jets lists
+from fit_functions import fit_fake_factor
+from save_correctionlib import *
+
+
+# Set ROOT to batch mode (no GUI popping up)
+ROOT.gROOT.SetBatch(True)
+
+# -------------------------------
+# Utility Functions
+# -------------------------------
+def create_th1d_histogram(name, bin_edges, bin_contents, bin_uncertainties):
+    """
+    Create a ROOT TH1D histogram from bin contents and uncertainties.
+    """
+    n_bins = len(bin_contents)
+    th1d = ROOT.TH1D(name, name, n_bins, bin_edges[0], bin_edges[-1])
+    
+    for i, (content, uncertainty) in enumerate(zip(bin_contents, bin_uncertainties), start=1):
+        if content > 1.0e-5 and uncertainty < 1:
+            th1d.SetBinContent(i, content)
+            th1d.SetBinError(i, uncertainty)
+    
+    return th1d
+
+
+def load_pkl_file(file_path):
+    """Load a pickle file containing the ratio data."""
+    with open(file_path, "rb") as file:
+        return pickle.load(file)
+
+
+def ensure_directory(directory):
+    """Ensure a directory exists."""
+    os.makedirs(directory, exist_ok=True)
+
+
+def configure_directories(config):
+    """Configure and return directories and necessary paths for each DM and Njet combination."""
+    ERA = config["era"]
+    CORRECTION_TYPE = config["correction_type"] 
+    VARIABLE = config["variable"]
+    HIST_NAME = config["hist_name"]
+    DM_LIST = config["dm"]
+    N_JETS_LIST = config["n_jets"]
+    
+    if VARIABLE == "met_var_qcd_h1":
+        PT_RANGE = (-1.5, 1.5)
+    else:
+        PT_RANGE = (35, 100)
+
+    OUTPUT_DIR_BASE = f"/afs/cern.ch/user/o/oponcet/private/analysis/CPinHToTauTau/script_FF/fake_factor_derivation/outputs/{ERA}/{CORRECTION_TYPE}"
+    ensure_directory(OUTPUT_DIR_BASE)
+
+    # Handle all combinations of DM and n_jets
+    categories = list(product(DM_LIST, N_JETS_LIST))
+    
+    input_files = []
+    output_dirs = []
+    categories_str = []
+
+    for dm, n_jets in categories:
+        CATEGORY, INPUT_FILE = get_input_file(dm, n_jets, config, VARIABLE,CORRECTION_TYPE)
+        output_dir = os.path.join(OUTPUT_DIR_BASE, f"{CATEGORY}")
+        ensure_directory(output_dir)
+
+        input_files.append(INPUT_FILE)
+        output_dirs.append(output_dir)
+        categories_str.append(CATEGORY)
+
+    return input_files, output_dirs, HIST_NAME, categories_str, PT_RANGE
+
+
+def get_input_file(dm, n_jets, config, VARIABLE,CORRECTION_TYPE):
+    """Return the input file path based on dm, n_jets, and the given config."""
+    if dm == -1 and n_jets == -1:
+        CATEGORY = "inclusive"
+        INPUT_FILE = f"{config['input_base_path']}/RATIO_{VARIABLE}_{CATEGORY}.pkl"
+    elif dm == -1 and n_jets != -1:
+        CATEGORY = n_jets
+        INPUT_FILE = f"{config['input_base_path']}/RATIO_{VARIABLE}_{CATEGORY}.pkl"
+    elif dm != -1 and n_jets == -1:
+        CATEGORY = dm
+        INPUT_FILE = f"{config['input_base_path']}/RATIO_{VARIABLE}_{CATEGORY}.pkl"
+    elif dm != -1 and n_jets != -1:
+        CATEGORY = f"{dm}_{n_jets}"
+        INPUT_FILE = f"{config['input_base_path']}/{CORRECTION_TYPE}_{VARIABLE}_dm_{dm}_njet_{n_jets}.pkl" 
+    else:
+        raise ValueError("Invalid DM and N_JETS values")
+
+    return CATEGORY, INPUT_FILE
+
+
+def save_root_file(OUTPUT_DIR, th1d, HIST_NAME, fit, h_uncert, ratio_hist, config, CATEGORY):
+    """Save the histogram, fit results, and uncertainties to a ROOT file."""
+    output_root_file = os.path.join(OUTPUT_DIR, f"{config['correction_type']}_{config['era']}_{CATEGORY}.root")
+    output_file = ROOT.TFile(output_root_file, "RECREATE")
+    th1d.Write(HIST_NAME)
+    fit.Write("fit")
+    h_uncert.Write("h_uncert")
+    ratio_hist.Write("ratio_hist")
+    output_file.Close()
+    return output_root_file
+
+
+def plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, CATEGORY, output_root_file):
+    """Create and save the plot results."""
+    canvas = ROOT.TCanvas("Extrapolation Correction", "Extrapolation Correction", 800, 600)
+    ratio_hist.Draw("EP")
+    ratio_hist.SetLineColor(ROOT.kBlack)
+    ratio_hist.SetMarkerStyle(20)
+    
+    ratio_hist.SetTitle("Extrapolation Correction for inclusive in DM and Njets")
+    ratio_hist.GetYaxis().SetTitle("Extrapolation Correction")
+    ratio_hist.GetXaxis().SetTitle("p_{T} (GeV)")
+    ratio_hist.GetYaxis().SetRangeUser(0, 1.0)
+
+    h_uncert.Draw("E3 SAME")
+    h_uncert.SetFillColorAlpha(ROOT.kAzure + 7, 0.3)
+    h_uncert.SetLineColor(ROOT.kAzure + 7)
+    h_uncert.SetLineWidth(2)
+
+    fit.Draw("SAME")
+    fit.SetLineColor(ROOT.kAzure + 7)
+
+    legend = ROOT.TLegend(0.15, 0.75, 0.35, 0.9)
+    legend.AddEntry(ratio_hist, "Extrapolation Correction", "EP")
+    legend.AddEntry(fit, "Fit Result", "L")
+    legend.AddEntry(h_uncert, "68% CL (Uncertainties)", "F")
+    legend.SetBorderSize(0)
+    legend.SetFillStyle(0)
+    legend.SetTextSize(0.03)
+    legend.Draw()
+
+    output_image_base = output_root_file.replace(".root", "")
+    canvas.SaveAs(f"{output_image_base}_FullPlot.root")
+    canvas.SaveAs(f"{output_image_base}_FullPlot.png")
+    canvas.SaveAs(f"{output_image_base}_FullPlot.pdf")
+
+    # -------------------------------
+    # Adjust Fit Box and Save Details
+    # -------------------------------
+    ROOT.gStyle.SetOptStat(0)  # Disable stat box
+    ROOT.gStyle.SetStatY(0.8)
+    ROOT.gStyle.SetStatX(0.7)
+    ROOT.gStyle.SetStatW(0.15)
+    ROOT.gStyle.SetStatH(0.15)
+    ROOT.gStyle.SetOptFit(1)   # Show fit info
+
+    canvas.SaveAs(f"{output_image_base}_FitDetails.root")
+    canvas.SaveAs(f"{output_image_base}_FitDetails.png")
+    canvas.SaveAs(f"{output_image_base}_FitDetails.pdf")
+
+    return canvas
+
+def rebin_to_custom_bins(hist, custom_bins):
+    """
+    Rebins a histogram to custom bin ranges and normalizes content to bin widths.
+
+    Args:
+        hist (ROOT.TH1): The original histogram to rebin.
+        custom_bins (list): List of custom bin edges (e.g., [40, 44, 48, ...]).
+    
+    Returns:
+        ROOT.TH1: A new histogram with custom bins and normalized bin content.
+    """
+
+    print("###############################################")
+    # Ensure custom_bins is a valid list
+    if len(custom_bins) < 2:
+        raise ValueError("custom_bins must contain at least two values to define bin ranges.")
+    
+    # Convert to array format for ROOT
+    bin_edges_array = array('d', custom_bins)
+    n_bins = len(custom_bins) - 1
+
+    # Create a new histogram with custom binning
+    rebinned_hist = ROOT.TH1F(
+        f"{hist.GetName()}_rebinned",
+        hist.GetTitle(),
+        n_bins,
+        bin_edges_array
+    )
+
+    for i in range(0,n_bins):
+        new_bin_low = bin_edges_array[i] # low edge of the bin
+        new_bin_high = bin_edges_array[i + 1] # high edge of the bin
+        # print(f"new_bin_low: {new_bin_low}, new_bin_high: {new_bin_high}")
+        # Find the corresponding bin in the original histogram
+        new_bin_content = 0
+        n_bin_merged = 0
+        new_bin_error_sq = 0  # Sum of squared errors for the new bin
+
+        # Loop over the original histogram bins
+        for j in range(1, hist.GetNbinsX() + 1):
+            bin_center = hist.GetBinCenter(j)
+            bin_content = hist.GetBinContent(j)
+            bin_error = hist.GetBinError(j)
+
+            # Check if the bin center is within the new bin range
+            if new_bin_low <= bin_center < new_bin_high and bin_content != 0:
+                # Add bin content to the new histogram
+                n_bin_merged += 1
+                new_bin_content += bin_content
+                new_bin_error_sq += bin_error ** 2
+                # print(f"bin_center: {bin_center}, bin_content: {bin_content}")
+
+        # Skip empty bins
+        if n_bin_merged == 0:
+            continue
+        bin_width = new_bin_high - new_bin_low    
+        new_bin_content /= n_bin_merged # normalize to the bin width
+        new_bin_error = (new_bin_error_sq**0.5) / n_bin_merged  # Propagate uncertainty
+        # print(f"new_bin_content: {new_bin_content} for bin center : {rebinned_hist.GetBinCenter(i + 1)} ")
+        rebinned_hist.SetBinContent(i + 1, new_bin_content)
+        rebinned_hist.SetBinError(i + 1, new_bin_error)
+
+    return rebinned_hist
+
+def save_json_correction(fit, fit_up, fit_down, ratio_hist, output_root_file, config, PT_RANGE, dm, njet):
+    """Save the fit results to a JSON file using the correctionlib format."""
+    output_json_file = output_root_file.replace(".root", ".json")
+    fit_formula = str(fit.GetExpFormula("P"))  # Explicitly cast to a Python string
+
+    fit_up_formula = str(fit_up.GetExpFormula("P"))
+    fit_down_formula = str(fit_down.GetExpFormula("P"))
+
+    print(f"Fit formula: {fit_formula}")
+    print(f" fit up formula: {fit_up_formula}")
+    print(f" fit down formula: {fit_down_formula}")
+
+    save_to_correctionlib_with_fit(ratio_hist, output_json_file, dm, njet, fit_formula, fit_up_formula, fit_down_formula, 
+                                   config['correction_type'], config['variable'], PT_RANGE[0], PT_RANGE[1])
+
+# -------------------------------
+# Main Function
+# -------------------------------
+def main(args):
+
+    config = args.config
+    if not os.path.exists(args.config):
+        raise ValueError(f"Configuration file {args.config} does not exist.")
+    
+    print("Using configuration file: %s"%(args.config))
+
+    # Load configuration from JSON file
+    with open(args.config, "r") as config_file:
+        config = json.load(config_file)
+
+    # Assign configuration values and directories
+    input_files, output_dirs, HIST_NAME, categories, PT_RANGE = configure_directories(config)
+    
+    for INPUT_FILE, OUTPUT_DIR, CATEGORY in zip(input_files, output_dirs, categories):
+        print(f"Processing category: {CATEGORY}")
+
+        dm = CATEGORY.split("_")[0]
+        njet = CATEGORY.split("_")[1]
+
+
+        # -------------------------------
+        # Load Data
+        # -------------------------------
+        ratio = load_pkl_file(INPUT_FILE)
+        h = ratio[0, :, :]
+
+        bin_edges = h.axes[1].edges
+        bin_contents = h.values().flatten()
+        bin_uncertainties = np.sqrt(h.variances()).flatten()
+
+        print(f"Loaded data from {INPUT_FILE}")
+
+        th1d = create_th1d_histogram(HIST_NAME, bin_edges, bin_contents, bin_uncertainties)
+        th1d.SetMinimum(0)
+        th1d.SetMaximum(1.6)
+
+        custom_bins = [35,40,45,50,55,60,65,70,80,120,200] # Custom binning for hcand_1_pt
+        th1d = rebin_to_custom_bins(th1d, custom_bins)
+
+
+        # -------------------------------
+        # Fit Fake Factor & Save Fit Outputs
+        # -------------------------------
+        fit_range = (35, 200)
+        fit, h_uncert, ratio_hist, fit_up, fit_down = fit_fake_factor(th1d, *fit_range, usePol1=False, polOnly=3)
+
+        # Save ROOT file
+        output_root_file = save_root_file(OUTPUT_DIR, th1d, HIST_NAME, fit, h_uncert, ratio_hist, config, CATEGORY)
+        
+        # Plot Results
+        canvas = plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, CATEGORY, output_root_file)
+
+        # Save to JSON
+        save_json_correction(fit, fit_up, fit_down, ratio_hist, output_root_file, config, PT_RANGE, dm, njet)
+
+
+# -------------------------------
+# Call the main function if script is executed
+# -------------------------------
+if __name__ == '__main__':
+
+    argv = sys.argv
+    parser = ArgumentParser()
+    parser.add_argument('-c', '--config', dest='config', type=str, default='script_FF/fake_factor_derivation/src/config/config_default.json', action='store', help="set config file")
+    args = parser.parse_args()
+
+    main(args)
+
+    print(">>>\n>>> done\n")
---
+++ script_FF/fake_factor_derivation/src/fit_functions.py
@@ -0,0 +1,280 @@
+'''
+Author : @oponcet
+Date : 06-12-2024
+Script with different useful function:
+    - FitFakeFactors: fit the fake factor with landau or pol1 function
+'''
+
+import ROOT
+import math
+
+def fit_fake_factor(h, xmin, xmax, usePol1=False, polOnly=None):
+    """
+    Fit the fake factor with a Landau function and/or polynomial functions.
+
+    Parameters:
+    - h: TH1D histogram to fit.
+    - usePol1: bool, optional, default=False. If True, include a pol1 term in the fit.
+    - polOnly: int, optional, default=None. If specified, fit only with pol0 or pol1 function:
+        - 0: Use pol0 function only.
+        - 1: Use pol1 function only.
+        - 2: Use pol2 function only.
+        - 3: Use pol3 function only.
+        - None: Use Landau or Landau+pol1 by default.
+
+    Returns:
+    - fit: TF1 fit function with the final parameters.
+    - h_uncert: TH1D histogram representing the fit uncertainties.
+    - h: TH1D histogram, with bins adjusted during fitting.
+    """
+    if not h or h.GetNbinsX() == 0:
+        raise ValueError("Input histogram is invalid or empty.")
+
+    print(f"Fitting histogram {h}")
+    
+    # Prepare an uncertainty histogram
+    h_uncert = ROOT.TH1D(h.GetName() + '_uncert', "", 1000, h.GetBinLowEdge(1), h.GetBinLowEdge(h.GetNbinsX() + 1))
+    
+    # Define the fit functions
+    f1 = ROOT.TF1("f1", "landau", xmin, xmax)
+    f2 = ROOT.TF1("f2", "[0]*TMath::Landau(x,[1],[2])+[3]", xmin, xmax)
+    if usePol1:
+        f2 = ROOT.TF1("f2", "[0]*TMath::Landau(x,[1],[2])+[3]+[4]*x", xmin, xmax)
+    
+    if polOnly == 0:
+        f1 = ROOT.TF1("f1", "pol0", xmin, xmax)
+        f2 = ROOT.TF1("f2", "pol0", xmin, xmax)
+    elif polOnly == 1:
+        f1 = ROOT.TF1("f1", "pol1", xmin, xmax)
+        f2 = ROOT.TF1("f2", "pol1", xmin, xmax)
+    elif polOnly == 2:
+        f1 = ROOT.TF1("f1", "pol2", xmin, xmax)
+        f2 = ROOT.TF1("f2", "pol2", xmin, xmax)
+    elif polOnly == 3:
+        f1 = ROOT.TF1("f1", "pol3", xmin, xmax)
+        f2 = ROOT.TF1("f2", "pol3", xmin, xmax)
+    elif polOnly == 4:
+        f1 = ROOT.TF1("f1", "pol4", xmin, xmax)
+        f2 = ROOT.TF1("f2", "pol4", xmin, xmax)
+    elif polOnly == -1: # use Gaussian approximation  exp(− ((x−mean)**2) / (2x sigma**2))
+        f1 = ROOT.TF1("f1", "[0]*exp(-0.5*((x-[1])/[2])^2)", xmin, xmax)  # Gaussian function without the linear term
+        f2 = ROOT.TF1("f2", "[0]*exp(-0.5*((x-[1])/[2])^2) + [3] + [4]*x", xmin, xmax)
+
+    
+    
+    # Reset histogram, keeping only bins with content > 0
+    h_clone = h.Clone()
+    h_clone.Reset()
+    for i in range(1, h.GetNbinsX() + 1):
+        content = h.GetBinContent(i)
+        error = h.GetBinError(i)
+        if content > 0:
+            h_clone.SetBinContent(i, content)
+            h_clone.SetBinError(i, error)
+    h = h_clone
+
+    # Fit logic
+    fit = None
+    if polOnly is None:
+        # Initial Landau fit for parameter seeding
+        h.Fit("f1", 'IR')
+        f2.SetParameter(0, f1.GetParameter(0))
+        f2.SetParameter(1, f1.GetParameter(1))
+        f2.SetParameter(2, f1.GetParameter(2))
+        f2.SetParameter(3, 0)  # Initial offset
+        if usePol1:
+            f2.SetParameter(4, 0)  # Initial slope
+        # Iterative fitting
+        rep = True
+        count = 0
+        while rep:
+            fitresult = h.Fit("f2", 'SIR')
+            rep = int(fitresult) != 0  # Repeat if fit fails      
+            if not rep or count > 100:
+                print(f"Fit converged after {count} iterations.")
+                print(f"Fit result: {fitresult}")
+                # Generate confidence intervals for uncertainty
+                ROOT.TVirtualFitter.GetFitter().GetConfidenceIntervals(h_uncert, 0.68)
+                fit = f2
+                # print(f"Final fit function: {fit.GetExpFormula('P')}")
+                # uncerts, fit_up, fit_down = DecomposeUncerts(fitresult, fit)
+                fit_up, fit_down, fit_nom= get_variated_fitfunction(fit, fitresult, h_uncert)
+                # print(f"Final fit up: {fit_up.GetExpFormula('P')}")
+                # print(f"Final fit down: {fit_down.GetExpFormula('P')}")
+                # print(f"Final fit nom: {fit_nom.GetExpFormula('P')}")
+                break
+            count += 1
+
+    else:
+        # Direct fit with specified polynomial
+        fitresult = h.Fit("f2", 'SIR')
+        print("fit1")
+        ROOT.TVirtualFitter.GetFitter().GetConfidenceIntervals(h_uncert, 0.68)
+        fit = f2
+        # uncerts, fit_up, fit_down = DecomposeUncerts(fitresult, fit)
+        # print(f"Final fit function: {fit.GetExpFormula('P')}")
+        fit_up, fit_down, fit_nom = get_variated_fitfunction(fit, fitresult, h_uncert)
+        # print(f"Final fit up: {fit_up.GetExpFormula('P')}")
+        # print(f"Final fit down: {fit_down.GetExpFormula('P')}")
+        # print(f"Final fit nom: {fit_nom.GetExpFormula('P')}")
+        fitresult = h.Fit("f2", 'SIR')
+
+
+    # Set range of h_uncert to match fit range
+    h_uncert.SetAxisRange(xmin, xmax)
+    f2.SetRange(xmin, xmax)
+
+    if fit is None:
+        raise RuntimeError("Fit did not converge after 100 iterations.")
+
+    
+    # Name and return the fit
+    fit.SetName(h.GetName() + '_fit')
+    return fit, h_uncert, h, fit_up, fit_down
+
+
+def fit_correction(h, func='pol1'):
+    """
+    Fit the correction with a pol1 function.
+    """
+
+    # Prepare an uncertainty histogram
+    h_uncert = ROOT.TH1D(h.GetName()+'_uncert',"",1000,h.GetBinLowEdge(1),h.GetBinLowEdge(h.GetNbinsX()+1))
+    
+    # Define the fit functions
+    f1 = ROOT.TF1("f1",func)
+    #  fit with the full functions
+    # repeat fit up to 100 times until the fit converges properly
+    rep = True
+    count = 0
+    while rep:
+        fitresult = h.Fit("f1",'SI')
+        rep = int(fitresult) != 0
+        if not rep or count>100:
+            ROOT.TVirtualFitter.GetFitter().GetConfidenceIntervals(h_uncert, 0.68)
+            fit = f1
+            break
+        count+=1
+    fit.SetName(h.GetName()+'_fit')
+    return fit, h_uncert
+
+def DecomposeUncerts(fitresult, fit):
+    # Decompose the uncertainties of the fit for each fit parameter
+    # from https://github.com/danielwinterbottom/TauSF/blob/main/python/fit_tools.py#L4-L43 thanks Danny
+
+    shifted_functions = []
+    # Decompose the covariance matrix into eigenvectors
+    cov = ROOT.TMatrixD(fitresult.GetCovarianceMatrix())
+    eig = ROOT.TMatrixDEigen(cov)
+    eigenvectors = eig.GetEigenVectors()
+
+    # Estimate uncertainty variations based on the eigenvectors
+    pars = ROOT.TVectorD(fit.GetNpar())
+    for i in range(fit.GetNpar()):
+        pars[i] = fit.GetParameter(i)
+    variances = eig.GetEigenValues()
+    transposed_eigenvectors = eigenvectors.Clone().T()
+
+    # Loop over the parameters
+    for i in range(fit.GetNpar()):
+
+        temp = ROOT.TVectorD(fit.GetNpar())
+        for j in range(fit.GetNpar()):
+            temp[j] = transposed_eigenvectors(i, j)
+
+
+        temp*=variances(i,i)**0.5
+        fit_up=fit.Clone()  
+        fit_down=fit.Clone() 
+
+        # shift each parameter of the function in turn
+        for j in range(fit.GetNpar()): 
+            p_uncert = temp[j] 
+            nom = fit.GetParameter(j)
+            p_up=nom+p_uncert   
+            p_down=nom-p_uncert   
+            fit_up.SetParameter(j,p_up)
+            fit_down.SetParameter(j,p_down)
+            fit_up.SetName(fit.GetName()+'_uncert%i_up' %i)
+            fit_down.SetName(fit.GetName()+'_uncert%i_down' %i)
+
+            shifted_functions.append(('uncert%i' % i, fit_up, fit_down))
+        print(">>>>>>>>>>>>>>>>>>>>>>>>>>>>> parameter %i nominal is %f" % (i, fit.GetParameter(i)))
+        print(f"decompose nominal = {fit.GetExpFormula('P')}")
+        print(f"decompose fit up = {fit_up.GetExpFormula('P')}")
+        print(f"decompose fit down = {fit_down.GetExpFormula('P')}")
+
+
+    print(f"Final fit up = {fit_up.GetExpFormula('P')}")
+    print(f"Final fit down = {fit_down.GetExpFormula('P')}")
+
+    print(f"Decomposed uncertainties:" , shifted_functions)
+    return shifted_functions, fit_up, fit_down
+
+import ROOT
+
+def get_variated_fitfunction(fit, fitresult, h_uncert):
+    '''
+    Function that returns the fit_up (TF1) and fit_down (TF1) functions by extracting the upper and lower 
+    bands from the uncertainty envelope and fitting them with the same fit function.
+    '''
+    
+    # Number of bins in h_uncert should match the number of parameters in the fit result
+    fit_up = fit.Clone("fit_up")  # Clone the original fit for the upper bound
+    fit_down = fit.Clone("fit_down")  # Clone the original fit for the lower bound
+    fit_nom = fit.Clone("fit_nom")  # Clone the original fit for the nominal value
+    
+    # We need to create histograms for the upper and lower bounds
+    # Create new histograms to store the upper and lower bounds
+    h_up = h_uncert.Clone("h_up")
+    h_down = h_uncert.Clone("h_down")
+    
+    # Extract the upper and lower bounds from the uncertainty envelope
+    for i in range(1, h_uncert.GetNbinsX() + 1):
+        # Get the value of the uncertainty band at bin i
+        envelop_value = h_uncert.GetBinContent(i)
+        envelop_error = h_uncert.GetBinError(i)
+        
+        # Set the upper and lower bounds of the uncertainty envelope
+        h_up.SetBinContent(i, envelop_value + envelop_error)   # Upper bound
+        h_down.SetBinContent(i, envelop_value - envelop_error)  # Lower bound
+
+        # Set error of upper and lower bounds to 0
+        h_up.SetBinError(i, 0)
+        h_down.SetBinError(i, 0)
+    
+    # Fit the upper and lower bounds with the same fit function (same functional form as the original fit)
+    fit_up_result = h_up.Fit(fit_up.GetName(), "S")  # S for 'Save' fit result
+    fit_down_result = h_down.Fit(fit_down.GetName(), "S")  # S for 'Save' fit result
+
+    fit_nom_result = h_uncert.Fit(fit.GetName(), "S")  # S for 'Save' fit result
+
+    # # write in a root file
+    # output_file = ROOT.TFile("fit.root", "RECREATE")
+    # fit_nom.Write()
+    # fit_up.Write()
+    # fit_down.Write()
+
+    # # plot theme on the same canvas
+    # canvas = ROOT.TCanvas("canvas", "canvas", 800, 600)
+
+    # h_uncert.Draw("E3") 
+    # h_uncert.SetFillColorAlpha(ROOT.kAzure + 7, 0.3)
+
+    # fit_nom.Draw("SAME")
+    # fit_up.Draw("SAME")
+    # fit_down.Draw("SAME")
+
+    # h_up.Draw("P SAME")
+    # h_down.Draw("P SAME")
+
+    # canvas.Write()
+
+    # canvas.SaveAs("fit.png")
+
+    # output_file.Close()
+    
+    return fit_up, fit_down, fit_nom
+
+
+    
