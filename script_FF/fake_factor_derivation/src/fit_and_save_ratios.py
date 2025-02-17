8c4eadfaa4a727e044d8372069ce907a51a71ae0
oponcet <oceane.poncet@cern.ch>
2025-02-17 16:47:13 +0100

    add possibility to combine year based on lumi

--- script_FF/fake_factor_derivation/src/fit_and_save_ratios.py
+++ script_FF/fake_factor_derivation/src/fit_and_save_ratios.py
@@ -9,6 +9,8 @@ from argparse import ArgumentParser
 from itertools import product  # To combine dm and n_jets lists
 from fit_functions import fit_fake_factor
 from save_correctionlib import *
+import cmsstyle as CMS
+ 
 
 
 # Set ROOT to batch mode (no GUI popping up)
@@ -99,9 +101,13 @@ def get_input_file(dm, n_jets, config, VARIABLE,CORRECTION_TYPE):
     return CATEGORY, INPUT_FILE
 
 
-def save_root_file(OUTPUT_DIR, th1d, HIST_NAME, fit, h_uncert, ratio_hist, config, CATEGORY):
+def save_root_file(OUTPUT_DIR, th1d, HIST_NAME, fit, h_uncert, ratio_hist, config, CATEGORY, combine=False):
     """Save the histogram, fit results, and uncertainties to a ROOT file."""
-    output_root_file = os.path.join(OUTPUT_DIR, f"{config['correction_type']}_{config['era']}_{CATEGORY}.root")
+    if combine == True:
+        era = config["era"].split("_")[0]  #only keep the year
+    else :
+        era = config["era"]
+    output_root_file = os.path.join(OUTPUT_DIR, f"{config['correction_type']}_{era}_{CATEGORY}.root")
     output_file = ROOT.TFile(output_root_file, "RECREATE")
     th1d.Write(HIST_NAME)
     fit.Write("fit")
@@ -111,7 +117,7 @@ def save_root_file(OUTPUT_DIR, th1d, HIST_NAME, fit, h_uncert, ratio_hist, confi
     return output_root_file
 
 
-def plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, CATEGORY, output_root_file):
+def plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, CATEGORY, output_root_file, lumi=1):
     """Create and save the plot results."""
     canvas = ROOT.TCanvas("Extrapolation Correction", "Extrapolation Correction", 800, 600)
     ratio_hist.Draw("EP")
@@ -139,6 +145,17 @@ def plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, CATEGORY, output_root_fi
     legend.SetFillStyle(0)
     legend.SetTextSize(0.03)
     legend.Draw()
+
+    ROOT.gStyle.SetOptFit(0) 
+
+    # Add luminosity label with cms style 
+    luminosity_label = ROOT.TLatex()
+    luminosity_label.SetNDC()
+    luminosity_label.SetTextFont(42)
+    luminosity_label.SetTextSize(0.03)
+    # display only 3 digits
+    lumi = "{:.2f}".format(lumi)
+    luminosity_label.DrawLatex(0.72, 0.91, f"{lumi} fb^{{-1}} (13.6 TeV)")
 
     output_image_base = output_root_file.replace(".root", "")
     canvas.SaveAs(f"{output_image_base}_FullPlot.root")
@@ -183,7 +200,7 @@ def rebin_to_custom_bins(hist, custom_bins):
     n_bins = len(custom_bins) - 1
 
     # Create a new histogram with custom binning
-    rebinned_hist = ROOT.TH1F(
+    rebinned_hist = ROOT.TH1D(
         f"{hist.GetName()}_rebinned",
         hist.GetTitle(),
         n_bins,
@@ -240,67 +257,160 @@ def save_json_correction(fit, fit_up, fit_down, ratio_hist, output_root_file, co
     save_to_correctionlib_with_fit(ratio_hist, output_json_file, dm, njet, fit_formula, fit_up_formula, fit_down_formula, 
                                    config['correction_type'], config['variable'], PT_RANGE[0], PT_RANGE[1])
 
+def merge_histograms_lumi_weighted(hist_list, lumi_list):
+    """Merge histograms using luminosity-weighted averaging."""
+    total_lumi = sum(lumi_list)
+
+    # verify that number of hsit un nb files
+    if len(hist_list) != len(lumi_list):
+        raise ValueError("Number of histograms and luminosities must be equal.")
+
+    merged_hist = hist_list[0].Clone()  # Clone first histogram to maintain structure
+    
+    # Reset values to zero
+    merged_hist.Reset()
+
+    hist1 = hist_list[0]
+    hist2 = hist_list[1]
+
+    lumi1 = lumi_list[0]
+    lumi2 = lumi_list[1]
+
+    # Test if they have the same binning
+    if hist1.GetNbinsX() != hist2.GetNbinsX():
+        raise ValueError("Histograms must have the same number of bins.")
+
+    # should have the same binning
+    for bin in range(1, hist1.GetNbinsX() + 1):
+        bin_content_hist1 = hist1.GetBinContent(bin)
+        bin_content_hist2 = hist2.GetBinContent(bin)
+
+        bin_uncert_hist1 = hist1.GetBinError(bin)
+        bin_uncert_hist2 = hist2.GetBinError(bin)
+
+        # Calculate the weighted average
+        merged_bin_content = (lumi1 * bin_content_hist1 + lumi2 * bin_content_hist2) / total_lumi
+        merged_bin_uncert = np.sqrt((lumi1 * bin_uncert_hist1**2 + lumi2 * bin_uncert_hist2**2) / total_lumi)
+
+        merged_hist.SetBinContent(bin, merged_bin_content)
+        merged_hist.SetBinError(bin, merged_bin_uncert)
+
+    return merged_hist
+
+
 # -------------------------------
 # Main Function
 # -------------------------------
 def main(args):
 
-    config = args.config
-    if not os.path.exists(args.config):
-        raise ValueError(f"Configuration file {args.config} does not exist.")
-    
-    print("Using configuration file: %s"%(args.config))
+    config_files = args.config
 
-    # Load configuration from JSON file
-    with open(args.config, "r") as config_file:
-        config = json.load(config_file)
+    nb_files = len(config_files)
 
-    # Assign configuration values and directories
-    input_files, output_dirs, HIST_NAME, categories, PT_RANGE = configure_directories(config)
-    
-    for INPUT_FILE, OUTPUT_DIR, CATEGORY in zip(input_files, output_dirs, categories):
-        print(f"Processing category: {CATEGORY}")
+    combined_ratios = {}
 
-        dm = CATEGORY.split("_")[0]
-        njet = CATEGORY.split("_")[1]
+    for config_file in config_files:
 
-
-        # -------------------------------
-        # Load Data
-        # -------------------------------
-        ratio = load_pkl_file(INPUT_FILE)
-        h = ratio[0, :, :]
-
-        bin_edges = h.axes[1].edges
-        bin_contents = h.values().flatten()
-        bin_uncertainties = np.sqrt(h.variances()).flatten()
-
-        print(f"Loaded data from {INPUT_FILE}")
-
-        th1d = create_th1d_histogram(HIST_NAME, bin_edges, bin_contents, bin_uncertainties)
-        th1d.SetMinimum(0)
-        th1d.SetMaximum(1.6)
-
-        custom_bins = [35,40,45,50,55,60,65,70,80,120,200] # Custom binning for hcand_1_pt
-        th1d = rebin_to_custom_bins(th1d, custom_bins)
-
-
-        # -------------------------------
-        # Fit Fake Factor & Save Fit Outputs
-        # -------------------------------
-        fit_range = (35, 200)
-        fit, h_uncert, ratio_hist, fit_up, fit_down = fit_fake_factor(th1d, *fit_range, usePol1=False, polOnly=3)
-
-        # Save ROOT file
-        output_root_file = save_root_file(OUTPUT_DIR, th1d, HIST_NAME, fit, h_uncert, ratio_hist, config, CATEGORY)
+        if not os.path.exists(config_file):
+            raise ValueError(f"Configuration file {config_file} does not exist.")
         
-        # Plot Results
-        canvas = plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, CATEGORY, output_root_file)
+        print("Using configuration file: %s"%(config_file))
 
-        # Save to JSON
-        save_json_correction(fit, fit_up, fit_down, ratio_hist, output_root_file, config, PT_RANGE, dm, njet)
+        # Load configuration from JSON file
+        with open(config_file, "r") as config_file:
+            config = json.load(config_file)
+        
+        lumi = config.get("luminosity", 1)
+
+        # Assign configuration values and directories
+        input_files, output_dirs, HIST_NAME, categories, PT_RANGE = configure_directories(config)
+        
+        for INPUT_FILE, OUTPUT_DIR, CATEGORY in zip(input_files, output_dirs, categories):
+            print(f"Processing category: {CATEGORY}")
+
+            dm = CATEGORY.split("_")[0]
+            njet = CATEGORY.split("_")[1]
 
 
+            # -------------------------------
+            # Load Data
+            # -------------------------------
+            ratio = load_pkl_file(INPUT_FILE)
+            h = ratio[0, :, :]
+
+            bin_edges = h.axes[1].edges
+            bin_contents = h.values().flatten()
+            bin_uncertainties = np.sqrt(h.variances()).flatten()
+
+            print(f"Loaded data from {INPUT_FILE}")
+
+            th1d = create_th1d_histogram(HIST_NAME, bin_edges, bin_contents, bin_uncertainties)
+            th1d.SetMinimum(0)
+            th1d.SetMaximum(1.6)
+   
+
+            custom_bins = [35,40,45,50,55,60,65,70,80,120,200] # Custom binning for hcand_1_pt
+            th1d = rebin_to_custom_bins(th1d, custom_bins)
+
+
+            if nb_files > 1:
+                # Store results for merging
+                key = (dm, njet)
+                if key not in combined_ratios:
+                    combined_ratios[key] = {"hists": [], "lumis": []}
+                 
+                combined_ratios[key]["hists"].append(th1d)
+                combined_ratios[key]["lumis"].append(lumi)
+
+
+            # -------------------------------
+            # Fit Fake Factor & Save Fit Outputs
+            # -------------------------------
+
+
+            fit_range = (35, 200)
+            fit, h_uncert, ratio_hist, fit_up, fit_down = fit_fake_factor(th1d, *fit_range, usePol1=False, polOnly=3)
+
+            # Save ROOT file
+            output_root_file = save_root_file(OUTPUT_DIR, th1d, HIST_NAME, fit, h_uncert, ratio_hist, config, CATEGORY)
+            
+            # Plot Results
+            canvas = plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, CATEGORY, output_root_file, lumi=lumi)
+
+            # Save to JSON
+            save_json_correction(fit, fit_up, fit_down, ratio_hist, output_root_file, config, PT_RANGE, dm, njet)
+
+    if nb_files > 1:
+        # Merge histograms for the same (dm, njet) category
+        for (dm, njet), data in combined_ratios.items():
+            print(f"Combining histograms for dm={dm}, njet={njet} using luminosity weighting")
+
+            print("data: ", data)
+
+            merged_hist = merge_histograms_lumi_weighted(data["hists"], data["lumis"])
+
+            combined_era = config["era"].split("_")[0]  #only keep the year
+            CORRECTION_TYPE = config["correction_type"] 
+
+            OUTPUT_DIR_BASE = f"/afs/cern.ch/user/o/oponcet/private/analysis/CPinHToTauTau/script_FF/fake_factor_derivation/outputs/{combined_era}/{CORRECTION_TYPE}"
+            ensure_directory(OUTPUT_DIR_BASE)
+
+            OUTPUT_DIR = os.path.join(OUTPUT_DIR_BASE, f"{dm}_{njet}")
+            ensure_directory(OUTPUT_DIR)
+
+
+            # Fit Fake Factor & Save Fit Outputs
+            fit_range = (35, 200)
+            fit, h_uncert, ratio_hist, fit_up, fit_down = fit_fake_factor(merged_hist, *fit_range, usePol1=False, polOnly=3)
+
+            # Save ROOT file
+            output_root_file = save_root_file(OUTPUT_DIR, merged_hist, HIST_NAME, fit, h_uncert, ratio_hist, config, f"{dm}_{njet}", combine=True)
+
+            # Plot Results
+            canvas = plot_results(fit, h_uncert, ratio_hist, OUTPUT_DIR, f"{dm}_{njet}", output_root_file, lumi=sum(data["lumis"]))
+
+            # Save to JSON
+            save_json_correction(fit, fit_up, fit_down, ratio_hist, output_root_file, config, PT_RANGE, dm, njet)
 # -------------------------------
 # Call the main function if script is executed
 # -------------------------------
@@ -308,7 +418,7 @@ if __name__ == '__main__':
 
     argv = sys.argv
     parser = ArgumentParser()
-    parser.add_argument('-c', '--config', dest='config', type=str, default='script_FF/fake_factor_derivation/src/config/config_default.json', action='store', help="set config file")
+    parser.add_argument('-c', '--config', dest='config', type=str, nargs='+', default=['script_FF/fake_factor_derivation/src/config/config_default.json'], action='store', help="set config file (multiple files supported)")
     args = parser.parse_args()
 
     main(args)
